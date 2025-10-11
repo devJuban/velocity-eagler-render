5a15eefd70cab2f10ecdd2782c0f2c6719055815
devJuban <jubanreal500@gmail.com>
2025-10-11 12:57:16 +0100

    final

--- main.sh
+++ main.sh
@@ -5,8 +5,6 @@ echo "Your MOTD (eagler only): $MOTD"
 echo "Your Max Players (eagler only): $MAXPLAYERS"
 echo "Your IP: $SERVER"
 
-echo "WARNING: KOYEB IS NO LONGER SUPPORTED, PLEASE USER RENDER INSTEAD!"
-
 sed -i 's/${MOTD}/'"$MOTD"'/g' velocity.toml
 sed -i 's/${MAXPLAYERS}/'"$MAXPLAYERS"'/g' velocity.toml
 sed -i 's/${SERVER}/'"$SERVER"'/g' velocity.toml
@@ -19,5 +17,5 @@ cd ../..
 
 while true; do curl http://127.0.0.1:14457 ; sleep 120; done &
 
-echo "Starting Velocity | For Koyeb"
+echo "Starting Velocity | For Render"
 java -Xmx512M -Xms512M -jar velocity.jar
